b9b24e01b9152372a0d75c70055761c278743efa
ramadhani rasyid <ramad0523@gmail.com>
2023-06-21 15:13:39 +0700

    Finalisasi clone halaman utama website

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,7 @@ menggunakan pendekatan Vite --}}
         <img class="img-thumbnail" src="{{ Vite::asset('resources/images/laravel.png') }}" alt="image">
         <div class="col-md-2 offset-md-5 mt-4">
             <div class="d-grid gap-2">
-                <a class="btn btn-dark" href="">Home</a>
+                <a class="btn btn-dark" href="home">Home</a>
             </div>
         </div>
     </div>
